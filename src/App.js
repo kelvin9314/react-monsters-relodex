da0b5efc0c94678569e5dbda5e91635d0a324ad7
kelvin <kelvin@program.com.tw>
2019-11-17 22:37:12 +0800

    22 Dynamic content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React , { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const initMonthers = [
+  {name: 'Frankenstein',id:1},
+  {name: 'Dracula',id:2},
+  {name: 'Zombie',id:3},
+]
+
 function App() {
+
+  const [monsters, setMonsters] = useState(initMonthers);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {monsters.map(monster => (
+       <h1 key={monster.id}>{monster.name}</h1>)
+     )}
     </div>
   );
 }
