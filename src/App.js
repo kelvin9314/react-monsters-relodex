5d983371dd3d50e2d799bf9ade958f4eb8311816
kelvin mok <kelvin9314@gmail.com>
2019-11-24 19:15:55 +0800

    32 search field

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
 
   const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -12,8 +13,13 @@ function App() {
       .then(users => setMonsters(users))
   }, []);
 
+  useEffect(() => {
+    console.log(searchField);
+  }, [searchField]);
+
   return (
     <div className="App">
+      <input type="search" placeholder="search monster" onChange={e => setSearchField(e.target.value)}/>
       <CardList monsters={monsters} /> 
     </div>
   );
