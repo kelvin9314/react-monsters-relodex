832e8e9b0fc8dd4465eb41ad516434813ada3ad9
kelvin mok <kelvin9314@gmail.com>
2019-12-01 19:31:08 +0800

    Updates

---
+++ precache-manifest.49c353bf4bae72db1e4a15e2f77da349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593a8bfb0c458c731dc5d072bb04b615",
+    "url": "/react-monsters-relodex/index.html"
+  },
+  {
+    "revision": "bcc598fb2cfbf8ba1c6c",
+    "url": "/react-monsters-relodex/static/css/main.e289c0d4.chunk.css"
+  },
+  {
+    "revision": "7ceec05b749a1f1f4e4b",
+    "url": "/react-monsters-relodex/static/js/2.8530cf25.chunk.js"
+  },
+  {
+    "revision": "bcc598fb2cfbf8ba1c6c",
+    "url": "/react-monsters-relodex/static/js/main.5191eb46.chunk.js"
+  },
+  {
+    "revision": "e9c79ef2b358269dd29e",
+    "url": "/react-monsters-relodex/static/js/runtime-main.7fb7d10d.js"
+  }
+]);
